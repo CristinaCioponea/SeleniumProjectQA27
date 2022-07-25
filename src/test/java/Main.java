ba78a23a583338d343124dc0b83095d3cff8c697
Cioponea Cristina <jula.cristina@yahoo.com>
2022-07-25 20:41:20 +0300

    Cristina2

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
     public static void main(String[] args) {
-      LoginTest loginTest = new LoginTest();
-      loginTest.validLoginTest();
+//      LoginTest loginTest = new LoginTest();
+//      loginTest.validLoginTest();
+      RegisterTest registerTest = new RegisterTest();
+      registerTest.validRegisterTest();
 
 
     }
