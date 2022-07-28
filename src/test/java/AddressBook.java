0f474c6467d6ea4f12d12de1156137bfce3ea191
Cioponea Cristina <jula.cristina@yahoo.com>
2022-07-28 21:06:43 +0300

    Cristina2

--- src/test/java/AddressBook.java
+++ src/test/java/AddressBook.java
@@ -10,6 +10,11 @@ public class AddressBook {
         driver.get("http://testfasttrackit.info/selenium-test");
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
         driver.findElement(By.cssSelector("#header-account > div > ul > li.first > a")).click();
+        driver.findElement(By.id("email")).sendKeys("jula.cristina@yahoo.com");
+        driver.findElement(By.id("pass")).sendKeys("cristina");
+        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-left.sidebar.col-left-first > div > div.block-content > ul > li:nth-child(3) > a")).click();
+
 
 
     }
