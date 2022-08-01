02e5da42d41d0d3ef829473534fcb91d5d89d468
Cioponea Cristina <jula.cristina@yahoo.com>
2022-08-01 20:17:27 +0300

    Cristina2

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,11 @@
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
+    @Test
     public void validLoginTest(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
