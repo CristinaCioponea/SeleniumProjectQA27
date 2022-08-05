ea9728e3075c32aef88283c52bb561db63953222
Cioponea Cristina <jula.cristina@yahoo.com>
2022-08-05 13:15:10 +0300

    Cristina2

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,3 +1,6 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,41 +9,54 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
-public class LoginTest {
-
+    public class LoginTest {
     private WebDriver driver;
 
-    @Test
-    public void validLoginTest() {
+    @Before
+    public void initialDriver() {
+
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.id("email")).sendKeys("jula.cristina@yahoo.com");
-        driver.findElement(By.id("pass")).sendKeys("cristina");
-        driver.findElement(By.id("send2")).click();
-        driver.quit();
     }
 
-    public void changeLanguageTest() {
+@Test
+        public void validLoginTest(){
+            driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+            driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+            driver.findElement(By.id("email")).sendKeys("jula.cristina@yahoo.com");
+            driver.findElement(By.id("pass")).sendKeys("cristina");
+            driver.findElement(By.id("send2")).click();
+            WebElement welcomeText = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong"));
+            Assert.assertEquals("Hello, Cioponea C Cristina!", welcomeText.getText());
+            WebElement myAccount = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-left.sidebar.col-left-first > div > div.block-title > strong > span"));
+            Assert.assertTrue(myAccount.isDisplayed());
+
+        }
+        @Test
+
+        public void changeLanguageTest(){
 //        select-language
-        Select selectLanguageDropdown = new Select(driver.findElement(By.id("select-language")));
-        selectLanguageDropdown.selectByVisibleText("German");
+            Select selectLanguageDropdown = new Select(driver.findElement(By.id("select-language")));
+            selectLanguageDropdown.selectByVisibleText("German");
+
+        }
+        @Test
+
+        public void hoverOverCategoryTest(){
+            Actions action = new Actions(driver);
+            WebElement womenCategory = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-1.first.parent > a"));
+            action.moveToElement(womenCategory).perform();
+            WebElement viewAll = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-1.first.parent > ul > li.level1.view-all > a"));
+            viewAll.click();
+        }
+        @After
+                public void closeDriver(){
+            driver.quit();
+        }
+
+
 
     }
 
-    @Test
-    public void hoverOverCategoryTest() {
-        Actions action = new Actions(driver);
-        WebElement womenCategory = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-1.first.parent > a"));
-        action.moveToElement(womenCategory).perform();
-        WebElement viewAll = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-1.first.parent > ul > li.level1.view-all > a"));
-        viewAll.click();
-
-
-    }
-
-
-}
